58203d4ef403abb144deaf60a3d661ed46652844
jvictorfig <j.v.f.f.s22@gmail.com>
2023-09-05 22:24:35 -0300

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <VAppBar app color="black" dark flat class="px-12">
+    <VCol>
+      <VBtn
+        ><VIcon color="yellow" left class="mr-2">fa-solid fa-code</VIcon>
+        SANTANA</VBtn
+      >
+    </VCol>
+    <VCol>
+      <VBtn text right @click="scroll('home')" class="text-yellow">Home</VBtn>
+      <VBtn text right @click="scroll('about')">Sobre</VBtn>
+      <VBtn text right @click="scroll('experience')">Experiência</VBtn>
+      <VBtn text right @click="scroll('education')">Educação</VBtn>
+      <VBtn text right @click="scroll('contact')">Contato</VBtn>
+    </VCol>
+  </VAppBar>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  methods: {
+    scroll(refName) {
+      const element = document.getElementById(refName);
+      element.scrollIntoView({ behavior: smooth });
+    },
+  },
+};
+</script>
---
+++ src/components/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <NavBar />
+</template>
+
+<script>
+import NavBar from "../NavBar.vue";
+
+export default {
+  name: "HomePage",
+  components: {
+    NavBar,
+  },
+};
+</script>
+
+<style scoped></style>
